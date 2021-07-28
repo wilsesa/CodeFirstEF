8495c2fba4296fe09a174429c65aa3788e2bb25d
wilsesa <wilson.sanchez14@hotmail.com>
2021-07-27 21:08:54 -0300

    Projeto CodeFirstEF utiliando MVC

---
+++ DAL/EFContext.cs
@@ -0,0 +1,21 @@
+﻿using CodeFirstEF.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CodeFirstEF.DAL
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("EFConnectionString") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Universidade> Universidades { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
